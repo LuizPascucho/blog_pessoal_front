80032405626525f0157946cdeb5fe909b64f0785
Luiz Pascucho <luizpascucho@bol.com.br>
2024-04-29 12:14:53 -0300

    criando rotas e componentes Navbar Footer e Home

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-  import React from "react";
-  import Home from './paginas/home/Home';
-  import './App.css';
+import React from 'react';
+import './App.css';
+import Home from './paginas/home/Home';
+import Navbar from './components/navBar/NavBar';
+import Footer from './components/footer/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './paginas/Login/Login';
 
-
-  function App() {
-    return (
-      <Home />
-    );
-  }
-
-  export default App;
+function App() {
+  return (
+    <>
+    <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+            </Routes>
+          </div>
+          <Footer />
+        </BrowserRouter>
+    </>
+);
+}
+export default App;
